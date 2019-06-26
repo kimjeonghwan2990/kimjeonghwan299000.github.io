c5bf4a4ebd041ebcdd0a5ffba4bdd3ae2087d30b
kimjenoghwan2991 <vmfosemwjd@likelion.org>
2019-06-26 23:41:49 +0900

    aboutform

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\은교\desktop\은교\학교\동아리\멋쟁이 사자처럼\세션\과제\11주차\likelion_7th_session14\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
